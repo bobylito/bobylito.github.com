58579e8ea38b5e0d57ecb3228f13216c19b2f9a7
Alex S <bobylito@users.noreply.github.com>
2025-08-08 20:50:44 +0300

    feat(blog): display drafts

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -5,7 +5,7 @@ function postFilter<T extends { data: CommonContent }>({ data }: T) {
   const isPublishTimePassed =
     Date.now() >
     new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin;
-  return !data.draft && (import.meta.env.DEV || isPublishTimePassed);
+  return import.meta.env.DEV || isPublishTimePassed;
 }
 
 export default postFilter;
